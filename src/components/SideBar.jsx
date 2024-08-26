7a0ec2648427aec72d81f03190afd5fe7d1f26e9
devsuthar777 <devsuthar777@gmail.com>
2024-08-26 20:42:06 +0530

    top track switch done

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,16 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { MusicContext } from '../context/musicContext';
 import { CiSearch } from "react-icons/ci";
 import SongCard from './SongCard';
 
 const SideBar = () => {
   const {songList} = useContext(MusicContext);
+  const [isTopTrack,setIsTopTrack] = useState(false);
   
   return (
     <div className='w-full h-full'>
       <div className='flex w-[258px] h-[32px] gap-[40px] font-inter
       text-[24px] leading-[32px] font-semibold
-      '><button className='text-[#FFFFFF] '>For You</button> <button className='text-[#FFFFFF] opacity-50'>Top Tracks</button></div>
+      '><button className={`text-[#FFFFFF] ${isTopTrack ? 'opacity-50' : 'opacity-100' }`} onClick={() => setIsTopTrack(false)}>For You</button> <button className={`text-[#FFFFFF] ${isTopTrack ? 'opacity-100' : 'opacity-50' }`} onClick={() => setIsTopTrack(true)}>Top Tracks</button></div>
       <div className='w-full flex bg-opacity-10 bg-white items-center rounded-lg  my-6'>
         <input placeholder='Search Song, Artist' className=' w-full py-[8px] px-[16px] text-white  bg-transparent outline-none  text-[18px]  leading-[28px]'>
         </input>
@@ -18,8 +19,8 @@ const SideBar = () => {
       </div>
       <div className='h-[720px]'>
         {
-          songList.map((song) => 
-            <SongCard song = {song}></SongCard>
+          songList.map((song,index) => 
+            !isTopTrack ? <SongCard song = {song} index={index}></SongCard> : (song.top_track ? <SongCard song = {song} index={index}></SongCard> : null )
           )
         }
       </div>
